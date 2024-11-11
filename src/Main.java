24dc775ce5efb19ab3c24ea93512df4b1b0853e8
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-11 08:40:36 -0500

    ini

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        int [] num = {3, 4, 7, 6, 2, 8 , 9, 4, 5, 1};
+        for (int x : num) {
+            if (x % 2 == 0) {
+                System.out.println(x);
+            }
+        }
+    }
 }
